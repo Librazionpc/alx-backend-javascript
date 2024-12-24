481b79e2365d6f8f78cc16e82b9d5a89521c028f
LibrazionPc <nwajeizion393@gmail.com>
2024-12-24 09:38:59 +0100

    fixing things

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,9 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface( {
-    input: process.stdin,
-    output: process.stdout
-})
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-    console.log(`Your name is: ${name}`);
-    rl.on('close', () => {
-        console.log('This important software is closing\n');
-    });
-
-    rl.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
